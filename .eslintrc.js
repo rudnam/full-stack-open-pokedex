e3fe389491fd2408307b03afa20686e1def92722
rudnam <francisalbarracin2@gmail.com>
2023-07-31 16:17:41 +0800

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "jest/globals": true,
     "cypress/globals": true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -16,14 +16,7 @@ module.exports = {
   },
   plugins: ["react", "jest", "cypress"],
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
     eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
     "no-console": "error",
     "react/prop-types": 0,
   },
