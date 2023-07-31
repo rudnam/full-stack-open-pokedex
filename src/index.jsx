ead679b5117b122aeb25a8ba5f32d2275c3f27b8
rudnam <francisalbarracin2@gmail.com>
2023-07-31 16:24:44 +0800

    Update dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />, document.getElementById("app"));
