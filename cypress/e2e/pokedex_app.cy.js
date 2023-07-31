d274a6f6af76f3cc50b63d452bddf28c18741ede
rudnam <francisalbarracin2@gmail.com>
2023-07-31 17:04:37 +0800

    11.9 Simple end to end -tests

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -3,7 +3,14 @@ describe("Pokedex", function () {
     cy.visit("http://localhost:5000");
     cy.contains("ivysaur");
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo.",
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
+  });
+  it("pokemon page can be navigated to", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("pikachu").click();
+    cy.contains("pikachu");
+    cy.contains("static");
+    cy.contains("lightning rod");
   });
 });
