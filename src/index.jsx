5755be43e6dc5698ff0d376d68424f6e65f15580
rudnam <francisalbarracin2@gmail.com>
2023-07-31 16:55:58 +0800

    11.9 Simple end to end -tests

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />, document.getElementById("app"));
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />, document.getElementById("app"));
